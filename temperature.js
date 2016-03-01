0e6c25b170dae4a22161c98c7ec4e83f34c598ca
alu0100698688 <alu0100698688@ull.edu.es>
2016-03-01 03:13:49 +0000

    Modificada en la rama correcta el código javascript

--- temperature.js
+++ temperature.js
@@ -2,22 +2,23 @@
 function calculate() {
   var result;
   var temp = original.value;
-  var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
-  
+  var regexp = /([\s]?[-+]?\d+(\.\d+)?(e[-+]?\d+)?)\s*(f$|fa$|fah$|fahr$|fahre$|fahren$|fahrenh$|fahrenhe$|fahrenhei$|fahrenheit$|c$|ce$|cel$|cels$|celsi$|celsiu$|celsius$)/i;
+
   var m = temp.match(regexp);
-  
+
   if (m) {
     var num = m[1];
-    var type = m[2];
+    var type = m[4];
     num = parseFloat(num);
-    if (type == 'c' || type == 'C') {
-      result = (num * 9/5)+32;
-      result = result.toFixed(1)+" Farenheit"
+    if (type[0] == 'c' || type[0] == 'C') {
+        result = (num * 9/5)+32;
+        result = result.toFixed(1)+" Farenheit"
     }
     else {
-      result = (num - 32)*5/9;
-      result = result.toFixed(1)+" Celsius"
+        result = (num - 32)*5/9;
+        result = result.toFixed(1)+" Celsius"
     }
+
     converted.innerHTML = result;
   }
   else {
